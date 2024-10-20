7e7d5cfbefa64255d4725e49c591102c688f025d
4th-avenue <quebec706@gmail.com>
2024-10-20 20:46:31 +0900

    작성한 Note가 없을 때 화면에 표시될 내용 만들고 Notes create 페이지 생성하고 Route

--- resources/views/livewire/notes/show-notes.blade.php
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -16,24 +16,32 @@ new class extends Component {
 
 <div>
     <div class="space-y-2">
-        <div class="grid grid-cols-2 gap-4 mt-12">
-            @foreach ($notes as $note)
-                <x-card wire:key='{{ $note->id }}'>
-                    <div class="flex justify-between">
-                        <a href="#"
-                            class="text-xl font-bold hover:underline hover:text-blue-500">{{ $note->title }}</a>
-                        <div class="text-xs text-gray-500">{{ \Carbon\Carbon::parse($note->send_date)->format('M-d-Y') }}
+        @if ($notes->isEmpty())
+            <div class="text-center">
+                <p class="text-xl font-bold">No notes yet</p>
+                <p class="text-sm">Let's create your first note to send.</p>
+                <x-button primary right-icon="plus" class="mt-6" href="{{ route('notes.create') }}" wire:navigate>Create note</x-button>
+            </div>
+        @else
+            <div class="grid grid-cols-2 gap-4 mt-12">
+                @foreach ($notes as $note)
+                    <x-card wire:key='{{ $note->id }}'>
+                        <div class="flex justify-between">
+                            <a href="#"
+                                class="text-xl font-bold hover:underline hover:text-blue-500">{{ $note->title }}</a>
+                            <div class="text-xs text-gray-500">{{ \Carbon\Carbon::parse($note->send_date)->format('M-d-Y') }}
+                            </div>
                         </div>
-                    </div>
-                    <div class="flex items-end justify-between mt-4 space-x-1">
-                        <p class="text-xs">Recipient: <span class="font-semibold">{{ $note->recipient }}</span></p>
-                        <div>
-                            <x-mini-button rounded icon="eye" />
-                            <x-mini-button rounded icon="trash" />
+                        <div class="flex items-end justify-between mt-4 space-x-1">
+                            <p class="text-xs">Recipient: <span class="font-semibold">{{ $note->recipient }}</span></p>
+                            <div>
+                                <x-mini-button rounded icon="eye" />
+                                <x-mini-button rounded icon="trash" />
+                            </div>
                         </div>
-                    </div>
-                </x-card>
-            @endforeach
-        </div>
+                    </x-card>
+                @endforeach
+            </div>
+        @endif
     </div>
 </div>
