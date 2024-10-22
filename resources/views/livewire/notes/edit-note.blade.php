7897398030dc6b16541ac8eeadcc0ac95dd1fe10
4th-avenue <quebec706@gmail.com>
2024-10-22 12:03:12 +0900

    NotePolicy를 이용해 본인이 작성한 Note의 수정 페이지만 갈 수 있게 하기

--- resources/views/livewire/notes/edit-note.blade.php
+++ resources/views/livewire/notes/edit-note.blade.php
@@ -1,14 +1,23 @@
 <?php
 
+use App\Models\Note;
 use Livewire\Volt\Component;
 use Livewire\Attributes\Layout;
 
 new
 #[Layout('layouts.app')]
 class extends Component {
-    //
+    public Note $note;
+
+    public function mount(Note $note)
+    {
+        $this->authorize('update', $note);
+        $this->fill($note);
+    }
 }; ?>
 
-<div>
-    Hi there
+<div class="space-y-2">
+    <p>{{ $note->title }}</p>
+    <p>{{ $note->id }}</p>
+    <p>{{ $note->user->email }}</p>
 </div>
