e26b88ecfb87fec3b504d8e90aabd796eb4ce14d
4th-avenue <quebec706@gmail.com>
2024-10-23 19:32:01 +0900

    send_date가 지난 Note는 수정 불가 & 개별 Note 페이지로 가는 링크 추가

--- resources/views/livewire/notes/show-notes.blade.php
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -50,9 +50,12 @@ new class extends Component {
                     <x-card wire:key='{{ $note->id }}'>
                         <div class="flex justify-between">
                             <div>
-                                <a href="{{ route('notes.edit', $note) }}" wire:navigate
-                                    class="text-xl font-bold hover:underline hover:text-blue-500">{{ $note->title }}</a>
-                                    <p class="mt-2 text-xs">{{ Str::limit($note->body, 50) }}</p>
+                                @can('update', $note)
+                                    <a href="{{ route('notes.edit', $note) }}" wire:navigate
+                                        class="text-xl font-bold hover:underline hover:text-blue-500">{{ $note->title }}</a>
+                                @else
+                                    <p class="text-xl font-bold text-gray-500">{{ $note->title }}</p>
+                                @endcan
                             </div>
                             <div class="text-xs text-gray-500">{{ \Carbon\Carbon::parse($note->send_date)->format('M-d-Y') }}
                             </div>
@@ -60,7 +63,7 @@ new class extends Component {
                         <div class="flex items-end justify-between mt-4 space-x-1">
                             <p class="text-xs">Recipient: <span class="font-semibold">{{ $note->recipient }}</span></p>
                             <div>
-                                <x-mini-button rounded icon="eye" />
+                                <x-mini-button rounded icon="eye" href="{{ route('notes.view', $note) }}" />
                                 <x-mini-button rounded icon="trash" wire:click="delete('{{ $note->id }}')" />
                             </div>
                         </div>
